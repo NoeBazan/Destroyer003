97fd6bd39dc9cd3f8e460c18eceef0709fce4c1b
NoeBazan <62730598+NoeBazan@users.noreply.github.com>
2024-09-22 21:03:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def resolver_ecuaciony(a1, b1, c1, a2, b2, c2):
 st.title("Generador y Solucionador de Ecuaciones de Primer Grado con dos incógnitas")
 
 # Botón para generar una nueva ecuación
-if st.button("Generar Ecuaciones"):
+if st.button("Proporcionar 2 Ecuaciones con 2 incógnitas"):
     generar_ecuacion1()
     generar_ecuacion2()
     st.write(f"La ecuación generada es: {st.session_state['a1']}.x + {st.session_state['b1']}.y = {st.session_state['c1']}")
@@ -69,7 +69,10 @@ if st.button("Mostrar Solución"):
     b2 = st.session_state['b2']
     c2 = st.session_state['c2']
     if a1 is not None and b1 is not None and a2 is not None and b2 is not None:
-        st.write(f"La solución para las ecuaciones: {a1}.x + {b1}.y = {c1}     y     {a2}.x + {b2}.y = {c2}")
+        st.write(f"La solución para las ecuaciones:")
+        st.write(f"{a1}.x + {b1}.y = {c1}")
+        st.write(f"y")
+        st.write(f"{a2}.x + {b2}.y = {c2}")
         solucionx = resolver_ecuacionx(a1, b1, c1, a2, b2, c2)
         soluciony = resolver_ecuaciony(a1, b1, c1, a2, b2, c2)
         if solucionx is not None:
